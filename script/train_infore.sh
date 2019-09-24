ddb3776319b3cec686e1131453197ce77fbd60f7
Vũ Đào Anh Tuấn <vudaoanhtuan@gmail.com>
2019-09-24 22:14:48 +0700

    fix training script

--- script/train_infore.sh
+++ script/train_infore.sh
@@ -1,8 +1,11 @@
+GD_MODEL_DIR="/content/drive/My Drive/AI/deep_speech/models"
+GD_DATA_DIR="/content/drive/My Drive/data/voice"
+
 cd ~
 
 echo "Download data"
-cp /content/drive/My\ Drive/data/voice/audiobooks.zip .
-cp /content/drive/My\ Drive/data/voice/transcribe.zip .
+cp "$GD_DATA_DIR/audiobooks.zip" .
+cp "$GD_DATA_DIR/transcribe.zip" .
 
 echo "Unzip data"
 unzip -P BroughtToYouByInfoRe -q audiobooks.zip 
@@ -13,9 +16,9 @@ if [ ! -e deepspeech.pytorch ]; then
     git clone https://github.com/vudaoanhtuan/deepspeech.pytorch.git
 fi
 mkdir deepspeech.pytorch/models
-LAST_CHECKPOINT="$(ls -1r /content/drive/My\ Drive/AI/deep_speech/models/deepspeech_*.pth.tar | head -n 1)"
-cp "$LAST_CHECKPOINT" deepspeech.pytorch/models
-LAST_CHECKPOINT="$(ls -t deepspeech.pytorch/models | head -n 1)"
+PREV_DIR=$(ls -1r $GD_MODEL_DIR | head -n 1)
+LAST_CHECKPOINT="$GD_MODEL_DIR/deepspeech_2.pth.tar"
+cp "$LAST_CHECKPOINT" deepspeech.pytorch/models/checkpoint.pth.tar
 
 echo "Start trainning"
 cd deepspeech.pytorch
@@ -23,7 +26,7 @@ python3 train.py \
     --train-manifest /root/train_manifest.csv \
     --val-manifest /root/test_manifest.csv \
     --labels-path vn_labels.json \
-    --continue-from models/$LAST_CHECKPOINT \
+    --continue-from models/checkpoint.pth.tar \
     --finetune \
     --batch-size 32 \
     --num-workers 2 \
@@ -32,5 +35,6 @@ python3 train.py \
     --epochs 2
 
 echo "Copy checkpoint to google drive"
-rm models/$LAST_CHECKPOINT
-cp models/* /content/drive/My\ Drive/AI/deep_speech/models/
+rm models/checkpoint.pth.tar
+NEW_DIR=$(date +'%Y-%m-%d_%H')
+cp -r models "$GD_MODEL_DIR/$NEW_DIR"

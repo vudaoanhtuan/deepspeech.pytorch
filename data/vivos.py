7f295d2eff191a68f3c70077f95f2e4ff5ec523f
Vũ Đào Anh Tuấn <vudaoanhtuan@gmail.com>
2019-12-04 21:21:41 +0700

    fix parent dir of wav file

--- data/vivos.py
+++ data/vivos.py
@@ -27,9 +27,9 @@ if __name__=='__main__':
 
     for line in data:
         v = line[:15].strip()
-        pd = v[:10]
+        parent_dir = v[:10]
         t = line[16:].strip().lower()
-        vp = os.path.join(voice_dir, pd, v+'.wav')
+        vp = os.path.join(voice_dir, parent_dir, v+'.wav')
         tp = os.path.join(transcribe_dir, v+'.txt')
         with open(tp, 'w') as f:
             f.write(t)
